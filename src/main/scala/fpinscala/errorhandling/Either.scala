98869faa96fe488bfcac48afe3965ecca6a42894
Norbert Scheller <norbert.scheller@actian.com>
2014-09-16 17:05:53 +0200

    initial Either commit

--- src/main/scala/fpinscala/errorhandling/Either.scala
+++ src/main/scala/fpinscala/errorhandling/Either.scala
@@ -2,6 +2,12 @@ package fpinscala.errorhandling
 
 import scala.{Option => _, Either => _, _} // hide standard library `Option` and `Either`, since we are writing our own in this chapter
 
-sealed trait Either[+E,+A]
+sealed trait Either[+E,+A] {
+  def map[B](f: A => B): Either[E,B] = ???
+  def flatMap[EE >: E,B](f: A => Either[EE,B]): Either[EE,B] = ???
+  def orElse[EE >: E,B >: A](b: Either[EE,B]): Either[EE,B] = ???
+  def map2[EE >: E,B,C](b: Either[EE,B])(f: (A,B) => C): Either[EE,C] = ???
+}
+
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
---
+++ src/main/scala/fpinscala/errorhandling/EitherWS.sc
@@ -0,0 +1,5 @@
+package fpinscala.errorhandling
+
+object EitherWS {
+  println("Welcome Either worksheet")             //> Welcome Either worksheet
+}
