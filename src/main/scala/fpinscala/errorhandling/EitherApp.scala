803bd4897f86067b39cb67ec52862b91f97ee78c
Norbert Scheller <norbert.scheller@actian.com>
2014-09-19 08:42:32 +0200

    closing ch04 commit

---
+++ src/main/scala/fpinscala/errorhandling/EitherApp.scala
@@ -0,0 +1,25 @@
+package fpinscala.errorhandling
+
+case class Person(name: String, age: Int)
+sealed class Name(val value: String)
+sealed class Age(val value: Int)
+  
+object EitherApp extends App {
+  def mkName(name: String): Either[String,Name] =
+    if (name == null || name == "") Left("name is empty")
+    else Right(new Name(name))
+    
+  def mkAge(age: Int): Either[String,Age] =
+    if (age < 0 || age > 120) Left("age is out of Range")
+    else Right(new Age(age))
+    
+  def mkPerson(name: String, age: Int): Either[String,Person] =
+    mkName(name).map2(mkAge(age))((n,a) => Person(n.value,a.value))
+  
+  override def main(args: Array[String]): Unit = {
+    println(mkPerson("Hans", 42))
+    println(mkPerson("",42))
+    println(mkPerson("Old", 121))
+    println(mkPerson("",121))
+  }
+}
--- src/main/scala/fpinscala/errorhandling/EitherWS.sc
+++ src/main/scala/fpinscala/errorhandling/EitherWS.sc
@@ -5,7 +5,9 @@ object EitherWS {
   
   val e1 = Right(2)                               //> e1  : fpinscala.errorhandling.Right[Int] = Right(2)
   val e2 = Right(3)                               //> e2  : fpinscala.errorhandling.Right[Int] = Right(3)
-  
+  val e3 = Left("Error")                          //> e3  : fpinscala.errorhandling.Left[String] = Left(Error)
   e1.map(_ + 1)                                   //> res0: fpinscala.errorhandling.Either[Nothing,Int] = Right(3)
   e2.map2(e1)(_ + _)                              //> res1: fpinscala.errorhandling.Either[Nothing,Int] = Right(5)
+  e2.map2(e3)_                                    //> res2: ((Int, Nothing) => Nothing) => fpinscala.errorhandling.Either[String,N
+                                                  //| othing] = <function1>
 }
