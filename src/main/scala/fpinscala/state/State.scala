dcc71a60aa7e00404785cc52ac8572b55595db2d
Norbert Scheller <norbert.scheller@actian.com>
2014-09-22 12:27:38 +0200

    ex 6.1, 6.2, 6.3, 6.4

--- src/main/scala/fpinscala/state/State.scala
+++ src/main/scala/fpinscala/state/State.scala
@@ -4,6 +4,8 @@ trait RNG {
   def nextInt: (Int, RNG)
 }
 
+import RNG._
+
 object RNG {
   case class SimpleRNG(seed: Long) extends RNG {
     def nextInt: (Int, RNG) = {
@@ -12,5 +14,57 @@ object RNG {
       val n = (nextSeed >>>16).toInt
       (n, nextRNG)
     }
+  }
+  
+  def randomPair(rng1: RNG): ((Int,Int),RNG) = {
+    val (i1,rng2) = rng1.nextInt
+    val (i2,rng3) = rng2.nextInt
+    
+    ((i1,i2),rng3)
+  }
+  
+  // exercise 6.1
+  def nonNegativeInt(rng: RNG): (Int,RNG) = {
+    val (i,r) = rng.nextInt
+    (if (i < 0) -(i+1) else i, r)
+  }
+  
+  // exercise 6.2
+  def double(rng: RNG): (Double,RNG) = {
+    val (i,r) = nonNegativeInt(rng)
+    val d = i.toDouble / (Int.MaxValue + 1)
+    (d,r)
+  }
+  
+  // exercise 6.3
+  def intDouble(rng: RNG): ((Int,Double),RNG) = {
+    val (i,rng2) = rng.nextInt
+    val (d,rng3) = double(rng2)
+    ((i,d),rng3)
+  }
+  
+  def doubleInt(rng: RNG): ((Double,Int),RNG) = {
+    val ((i,d),r) = intDouble(rng)
+    ((d,i),r)
+  }
+  
+  def double3(rng: RNG): ((Double,Double,Double),RNG) = {
+    val (d1,r2) = double(rng)
+    val (d2,r3) = double(r2)
+    val (d3,r4) = double(r3)
+    
+    ((d1,d2,d3),r4)
+  }
+  
+  // exercise 6.4
+  def ints(n: Int)(rng: RNG): (List[Int], RNG) = {
+    def go(c: Int, r: RNG, acc: List[Int]): (List[Int], RNG) =
+      if (c == 0) (acc,r)
+      else {
+        val (i,r2) = r.nextInt
+        go(c - 1, r2, i :: acc)
+      }
+  
+    go(n,rng,Nil)
   }
 }
--- src/main/scala/fpinscala/state/StateWS.sc
+++ src/main/scala/fpinscala/state/StateWS.sc
@@ -9,4 +9,13 @@ object StateWS {
                                                   //| rng2  : fpinscala.state.RNG = SimpleRNG(1059025964525)
   val (n2,rng3) = rng2.nextInt                    //> n2  : Int = -1281479697
                                                   //| rng3  : fpinscala.state.RNG = SimpleRNG(197491923327988)
+
+  val ((n3,n4),rng4) = randomPair(rng1)           //> n3  : Int = 16159453
+                                                  //| n4  : Int = -1281479697
+                                                  //| rng4  : fpinscala.state.RNG = SimpleRNG(197491923327988)
+  nonNegativeInt(rng4)                            //> res0: (Int, fpinscala.state.RNG) = (340305901,SimpleRNG(259172689157871))
+  double(rng4)                                    //> res1: (Double, fpinscala.state.RNG) = (-0.15846728393808007,SimpleRNG(259172
+                                                  //| 689157871))
+  ints(2)(rng1)                                   //> res2: (List[Int], fpinscala.state.RNG) = (List(-1281479697, 16159453),Simple
+                                                  //| RNG(197491923327988))
 }
