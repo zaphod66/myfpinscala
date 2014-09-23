9a1041bf5bf3ce94a09ceb785200c967a3124f1f
Norbert Scheller <norbert.scheller@actian.com>
2014-09-23 16:47:12 +0200

    initial ch07 commit

---
+++ src/main/scala/fpinscala/parallelism/Par.scala
@@ -0,0 +1,18 @@
+package fpinscala.parallelism
+
+import java.util.concurrent._
+
+object Par {
+  type Par[A] = ExecutorService => Future[A]
+  
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a) // `unit` is represented as a function that returns a `UnitFuture`, which is a simple implementation of `Future` that just wraps a constant value. It doesn't use the `ExecutorService` at all. It's always done and can't be cancelled. Its `get` method simply returns the value that we gave it.
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit) = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+}
