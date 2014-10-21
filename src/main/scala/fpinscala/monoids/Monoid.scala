4b40c8343a4cac67c1f45fe96645cec9c7f1c80e
Norbert Scheller <norbert.scheller@actian.com>
2014-10-21 15:53:09 +0200

    ex 10.12

--- src/main/scala/fpinscala/monoids/Monoid.scala
+++ src/main/scala/fpinscala/monoids/Monoid.scala
@@ -2,6 +2,8 @@ package fpinscala.monoids
 
 import fpinscala.parallelism.Par._
 
+import scala.language.higherKinds
+
 trait Monoid[A] {
   def op(a1: A, a2: A): A
   def zero: A
@@ -170,5 +172,42 @@ object Monoid {
       case Part(l, w, r) => unstub(l) + w + unstub(r)
     }
   }
+}
 
+trait Foldable[F[_]] {
+  def foldRight[A,B](as: F[A])(z: B)(f: (A,B) => B): B
+  def foldLeft[A,B](as: F[A])(z: B)(f: (B,A) => B): B
+  def foldMap[A,B](as: F[A])(f: A => B)(mb: Monoid[B]): B
+  def concatenate[A](as: F[A])(m: Monoid[A]): A =
+    foldLeft(as)(m.zero)(m.op)
+}
+
+// exercise 10.12
+object ListFoldable extends Foldable[List] {
+  def foldRight[A,B](as: List[A])(z: B)(f: (A,B) => B): B =
+    as.foldRight(z)(f)
+  def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B =
+    as.foldLeft(z)(f)
+  def foldMap[A, B](as: List[A])(f: A => B)(mb: Monoid[B]): B =
+    as.foldLeft(mb.zero)((b,a) => mb.op(b, f(a)))
+}
+
+object IndexedSeqFoldable extends Foldable[IndexedSeq] {
+  import Monoid._
+  
+  def foldLeft[A, B](as: IndexedSeq[A])(z: B)(f: (B, A) => B): B =
+    as.foldLeft(z)(f)
+  def foldRight[A, B](as: IndexedSeq[A])(z: B)(f: (A, B) => B): B =
+    as.foldRight(z)(f)
+  def foldMap[A, B](as: IndexedSeq[A])(f: A => B)(mb: Monoid[B]): B =
+    foldMapV(as,mb)(f)
+}
+
+object StreamFoldable extends Foldable[Stream] {
+  def foldLeft[A, B](as: Stream[A])(z: B)(f: (B, A) => B): B =
+    as.foldLeft(z)(f)
+  def foldRight[A, B](as: Stream[A])(z: B)(f: (A, B) => B): B =
+    as.foldRight(z)(f)
+  def foldMap[A, B](as: Stream[A])(f: A => B)(mb: Monoid[B]): B =
+    as.foldRight(mb.zero)((a,b) => mb.op(f(a), b))
 }
