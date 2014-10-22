53b8b44ae2653741ef2e2550e0a17291d97c36ea
Norbert Scheller <norbert.scheller@actian.com>
2014-10-22 11:04:50 +0200

    listFunctor

--- src/main/scala/fpinscala/monads/Monad.scala
+++ src/main/scala/fpinscala/monads/Monad.scala
@@ -4,4 +4,19 @@ import scala.language.higherKinds
 
 trait Functor[F[_]] {
   def map[A,B](fa: F[A])(f: A => B): F[B]
+  
+  def distribute[A,B](fab: F[(A,B)]): (F[A],F[B]) =
+    (map(fab)(_._1),map(fab)(_._2))
+    
+  def codistribute[A,B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
+    case Left(fa) => map(fa)(Left(_))
+    case Right(fb) => map(fb)(Right(_))
+  }
+}
+
+object Functor{
+  val listFunctor = new Functor[List] {
+    def map[A,B](as: List[A])(f: A => B): List[B] =
+      as map f
+  }
 }
